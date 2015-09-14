5e9ebdee157b840cdeafe01ba2663ecebd31f822
docente05 <jhopes.p@gmail.com>
2015-09-14 15:59:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/docente05/drawernavigationg2/ListViewAdapter.java
@@ -0,0 +1,63 @@
+package com.example.docente05.drawernavigationg2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by docente05 on 07/09/2015.
+ */
+public class ListViewAdapter extends BaseAdapter {
+    // Declare Variables
+    Context context;
+    String[] titulos;
+    int[] imagenes;
+    LayoutInflater inflater;
+
+    public ListViewAdapter(Context context, String[] titulos, int[] imagenes) {
+        this.context = context;
+        this.titulos = titulos;
+        this.imagenes = imagenes;
+    }
+
+    @Override
+    public int getCount() {
+        return titulos.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Declare Variables
+        TextView txtTitle;
+        ImageView imgImg;
+
+        //http://developer.android.com/intl/es/reference/android/view/LayoutInflater.html
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View itemView = inflater.inflate(R.layout.list_row, parent, false);
+
+        // Locate the TextViews in listview_item.xml
+        txtTitle = (TextView) itemView.findViewById(R.id.list_row_title);
+        imgImg = (ImageView) itemView.findViewById(R.id.list_row_image);
+
+        // Capture position and set to the TextViews
+        txtTitle.setText(titulos[position]);
+        imgImg.setImageResource(imagenes[position]);
+
+        return itemView;
+    }
+}
